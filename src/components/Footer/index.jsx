51092b010a38849a64eb359ee03e997fc489c0b5
Kasra Zafari <zafari.kasra@gmail.com>
2025-01-27 09:22:32 +0330

    Used my own components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import classes from "./index.module.css";
+
+const Footer = () => {
+
+    return (
+        <footer className={classes.footer}>
+            <h6>Designed and developed by Kasra</h6>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import classes from "./index.module.css"
+import { Link } from "react-router-dom";
+
+const Header = () => {
+
+    return (
+        <header className={classes.header}>
+            <div>
+                <img src="logo.png" alt="" />
+            </div>
+            <nav>
+                <Link to="/">
+                    <button>Home</button>
+                </Link>
+                <Link to="/products">
+                    <button>Products</button>
+                </Link>
+                <Link to="/contact">
+                    <button>Contact Us</button>
+                </Link>
+            </nav>
+            <div>
+                <button>Login</button>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
