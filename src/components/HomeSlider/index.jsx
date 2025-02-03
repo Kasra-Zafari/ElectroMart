bbfa9398bb4390bffddc29b71fcbef6520a79fdc
Kasra Zafari <zafari.kasra@gmail.com>
2025-02-03 16:09:43 +0330

    slider style

--- src/components/HomeSlider/index.jsx
+++ src/components/HomeSlider/index.jsx
@@ -3,6 +3,9 @@ import Slider from 'react-slick';
 import classes from './index.module.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import banner1 from '../../img/baner2.jpg';
+import banner2 from '../../img/baner1.jpg';
+import banner3 from '../../img/baner3.jpg';
 
 const HomeSlider = () => {
   const settings = {
@@ -11,12 +14,15 @@ const HomeSlider = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
 
   const products = [
-    { id: 1, name: 'Product 1', image: 'https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/1.png', link: '/product/1' },
-    { id: 2, name: 'Product 2', image: 'https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/1.png', link: '/product/2' },
-    { id: 3, name: 'Product 3', image: 'https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/1.png', link: '/product/3' },
+    { id: 1, name: 'Product 1', image: banner1, link: '/product/14' },
+    { id: 2, name: 'Product 2', image: banner2, link: '/product/4' },
+    { id: 3, name: 'Product 3', image: banner3, link: '/product/8' },
   ];
 
   return (
